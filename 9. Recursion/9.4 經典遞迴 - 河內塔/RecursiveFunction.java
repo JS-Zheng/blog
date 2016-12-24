0b0e66174594fa82416dad3b7f91a12fd4b6196e
Jasonnn <jasonnn331@gmail.com>
2016-12-25 05:12:43 +0800

    Fix typo

--- 9. Recursion/9.4 經典遞迴 - 河內塔/RecursiveFunction.java
+++ 9. Recursion/9.4 經典遞迴 - 河內塔/RecursiveFunction.java
@@ -9,9 +9,9 @@ public class RecursiveFunction implements HanoiTower {
         if (n == 1) { // 只有一個盤子
             System.out.println("將盤子從" + a + " 搬到" + c);
         } else {
-            hanoi(n - 1, a, c, b);
-            hanoi(1, a, b, c);
-            hanoi(n - 1, b, a, c);
+            hanoi(n - 1, a, c, b); // 先搬 n-1 個盤子 到 b (將 c 當中繼)
+            hanoi(1, a, b, c); // 將剩下的一個盤子 由 a 搬到 c
+            hanoi(n - 1, b, a, c); // 將剛剛 塔b 的 n-1 個盤子 再由 b 搬到 c (a 當中繼)
 
         }
     }
