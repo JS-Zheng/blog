1f6024185eb654ebcacc4dd1d426ae3aeabec1ce
Jasonnn <jasonnn331@gmail.com>
2017-01-20 04:54:55 +0800

    新增範例: 進制 的 前綴表示法

---
+++ 14. Numeral-System-Intro/前綴表示/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int dec = 9487; // 十進制
+        int oct = 022417; // 八進制 前綴表示法 (不建議使用)
+        int hex = 0x250F; // 十六進制 前綴表示法
+
+        System.out.println("十進制: " + dec);
+        System.out.println("八進制 前綴表示法: " + oct);
+        System.out.println("十六進制 前綴表示法: " + hex);
+    }
+}
+
+
+/*
+ * Result:
+ *
+ * 十進制: 9487
+ * 八進制 前綴表示法: 9487
+ * 十六進制 前綴表示法: 9487
+ *
+ */
