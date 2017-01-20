2ad7558bce824436dc10031fd132f57ea63e8c06
Jasonnn <jasonnn331@gmail.com>
2017-01-21 02:22:17 +0800

    補充 Underscores in Numeric Literals

--- 14. Numeral-System-Intro/前綴表示/Main.java
+++ 14. Numeral-System-Intro/前綴表示/Main.java
@@ -3,10 +3,19 @@ public class Main {
     public static void main(String[] args) {
 
         int dec = 9487; // 十進制
-        int oct = 022417; // 八進制 前綴表示法 (不建議使用)
-        int hex = 0x250F; // 十六進制 前綴表示法
+
+        int binary = 0b10010100001111; // 二進制 前綴表示法 0b
+
+        // (在 Java 7 後，可使用『底線』幫助閱讀，稱為 Underscores in Numeric Literals)
+        int binary2 = 0b10_0101_0000_1111; // 二進制 前綴表示法二 0b
+
+        int oct = 022417; // 八進制 前綴表示法 0 (不建議使用)
+
+        int hex = 0x250F; // 十六進制 前綴表示法 0x
 
         System.out.println("十進制: " + dec);
+        System.out.println("二進制 前綴表示法: " + binary);
+        System.out.println("二進制 前綴表示法二: " + binary2);
         System.out.println("八進制 前綴表示法: " + oct);
         System.out.println("十六進制 前綴表示法: " + hex);
     }
@@ -17,6 +26,8 @@ public class Main {
  * Result:
  *
  * 十進制: 9487
+ * 二進制 前綴表示法: 9487
+ * 二進制 前綴表示法二: 9487
  * 八進制 前綴表示法: 9487
  * 十六進制 前綴表示法: 9487
  *
